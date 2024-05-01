f913200040f68bbb9fd077eb5d56c0aa99955a8b
luqman1it <luqman1it@gmail.com>
2024-05-01 14:43:40 +0300

    add hover effect to button-group

--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -12,6 +12,7 @@ export default function Home() {
     const toggleDropDown = () => {
         setIsDropdownOpen(!isDropdownOpen);
     }
+
     return (
         <div id='home' className='la-home'>
             <div className="header">
