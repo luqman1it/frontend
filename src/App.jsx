c049a117f0834e8cea273ccd26944c6fcb7b49b9
rashait95 <rashabaakhras@gmial.com>
2024-05-02 05:08:21 +0300

    create project component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import './App.css'
+
 import Home from './Component/Home/Home'
 import About from './Component/About/About'
 import Project from './Component/Project/Project'
 import Contact from './Component/Contact/Contact'
+
+
+import './App.css'
+
+
 function App() {
 
 
   return (
     <>
-      <Home />
-      <About />
-      <Project />
-      <Contact />
+       <Home/>
+       <About/>
+       <Project/>
+       <Contact/>
     </>
   )
 }
--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,7 +1,105 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+
+import './Project.css'
+
+import data from './projectsData.json'
+
+
+// import required modules
+import { EffectCoverflow, Pagination } from 'swiper/modules';
+
+
+//declaring variables 
+
+const filter_buttons_names=['All','WebDesign','Apps','Graphics'];
 
 export default function Project() {
+
+    const [category,setCategory]=useState(filter_buttons_names);
+    const handleFilter = (e) =>{
+        let word=e.target.value;
+
+        if (word=='All'){
+            setCategory(filter_buttons_names);
+        }
+
+        if(word=='WebDesign'){
+            const filtered=filter_buttons_names.filter(item => item.name === 'Web Design');
+            setCategory(filtered)
+;        }
+         if(word=='Graphics'){
+           const filtered=filter_buttons_names.filter(item => item.name === 'Graphic');
+            setCategory(filtered)
+                 }
+         if(word=='App'){
+          const filtered=filter_buttons_names.filter(item => item.name === 'App');
+           setCategory(filtered)
+                          }
+    }
     return (
-        <div>Project</div>
+
+     <>
+    <div className='nav_filterbuttons'>
+        {filter_buttons_names.map((button,buttonId)=>{
+            return(
+                <button key={buttonId} value={button} onClick={handleFilter} className="filterbutton">{button}</button>
+            )
+        })}
+ 
+
+    </div>
+    <div className="projectswiper">
+    <Swiper
+        effect={'coverflow'}
+        grabCursor={true}
+        centeredSlides={true}
+        slidesPerView={'auto'}
+        coverflowEffect={{
+          rotate: 50,
+          stretch: 0,
+          depth: 100,
+          modifier: 1,
+          slideShadows: true,
+        }}
+        pagination={true}
+        modules={[EffectCoverflow, Pagination]}
+        className="mySwiper"
+      >
+    
+      {data.projects.map((project) => (
+        
+        <div key={project.id} className='projectcard'>
+         
+        <SwiperSlide>
+
+          <img src={project.img_url} />
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <button>github</button>
+        </SwiperSlide>
+        
+      
+
+        </div>
+        
+      ))}
+      </Swiper>
+    </div>
+
+      
+       
+
+
+  
+     </>
+        
     )
 }
