696a68a2c76f8056963ebac976dce2d00432baf3
luqman1it <luqman1it@gmail.com>
2024-05-06 14:24:53 +0300

    styling Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from './Component/Login/Login'
 import LandingBage from './Component/LandingBage'
 import DashboardProject from './Component/Dashboard/DashboardProject/DashboardProject'
 import DashboardMessage from './Component/Dashboard/DashboardMessage/DashboardMessage'
+import DashboardBox from './Component/Dashboard/DashboardBox/DashboardBox'
 
 function App() {
 
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingBage />} />
         <Route path="/dashboard" element={<Dashboard />} >
+          <Route path="box" element={<DashboardBox />} />
           <Route path="project" element={<DashboardProject />} />
           <Route path="message" element={<DashboardMessage />} />
 
