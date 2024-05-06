66db72a9de20732d4702749dfcb4e0b85f3b08e3
rashait95 <rashabaakhras@gmial.com>
2024-05-07 02:58:53 +0300

    create messages table using material ui

--- src/Component/Dashboard/DashboardMessage/DashboardMessage.jsx
+++ src/Component/Dashboard/DashboardMessage/DashboardMessage.jsx
@@ -1,7 +1,51 @@
-import React from 'react'
-import './DashboardMessage.css'
-export default function DashboardMessage() {
+import { Box, Typography } from '@mui/material'
+import { DataGrid } from '@mui/x-data-grid'
+import React, { useEffect, useMemo, useState } from 'react'
+
+import {getData} from './getdata'
+
+export default function DashboardMessage() { 
+  const [ messages,setMessages] = useState([]);
+  useEffect(() => {getData().then (data => {
+    setMessages(data);
+   
+  })},[]);
+
+  const columns = useMemo(() => {
+    return [
+      { field: 'id', headerName: 'ID', width: 70 },
+      { field: 'name', headerName: 'Name', width: 130 },
+      { field: 'email', headerName: 'Email', width: 130 },
+      { field:'subject', headerName: 'Subject', width: 130 },
+      { field:'message', headerName: 'Message', width: 130 },
+ 
+    ]
+
+  })
+
+
+ 
+
+
     return (
-        <div>DashboardMessage</div>
+      <Box 
+         sx={{ 
+          height:400,
+          width:'100%',
+          }}
+      >
+
+        <Typography variant='h2' component='h2' sx={{textAlign : 'center' ,mt:'5px' ,mb:'20px' }}>
+         Contact Messages
+        </Typography>
+        <DataGrid
+         columns={columns}
+         rows={messages}
+         sx={{textAlign : 'center'}}
+        >
+
+        </DataGrid>
+
+      </Box>
     )
 }
