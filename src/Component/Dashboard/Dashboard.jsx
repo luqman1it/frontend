6021f72caafb3570ebfcee30493ea53404efcc4d
luqman1it <luqman1it@gmail.com>
2024-05-10 01:58:38 +0300

    logout button

--- src/Component/Dashboard/Dashboard.jsx
+++ src/Component/Dashboard/Dashboard.jsx
@@ -4,17 +4,23 @@ import { Link, NavLink, Outlet, useNavigate } from 'react-router-dom'
 import userIcon from '../../images/abc.jpg'
 export default function Dashboard() {
     const navigate = useNavigate();
+
     useEffect(() => {
         if (!window.localStorage.getItem('token')) {
             navigate('/login');
         }
     }, []);
+
+    const handleLogOut = () => {
+        window.localStorage.removeItem('token');
+        navigate('/');
+    };
     return (
         <div className='dashboard'>
             <div className="dash-header">
                 <h2>Focal X</h2>
                 <div >
-                    <Link className='log-out' to='/'>LogOut</Link>
+                    <li onClick={handleLogOut} className='log-out' >LogOut</li>
                 </div>
             </div>
             <div className="dash-app">
