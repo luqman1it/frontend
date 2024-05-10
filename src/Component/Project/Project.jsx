3fc094e4bea55bfb9428876ded71e5f89fefe9c6
AYA2300 <ayasalleh2300@gmail.com>
2024-05-11 02:15:38 +0300

    ayaUpdate

--- src/Component/Project/Project.jsx
+++ src/Component/Project/Project.jsx
@@ -1,82 +1,87 @@
-import { useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
 
 // Import Swiper styles
-import 'swiper/css';
-import 'swiper/css/effect-coverflow';
-import 'swiper/css/pagination';
+import "swiper/css";
+import "swiper/css/effect-coverflow";
+import "swiper/css/pagination";
 
-import './Project.css'
+import "./Project.css";
 
-import { projects } from './projectsData.js';
-
+import { projects } from "./projectsData.js";
 
 // import required modules
-import { EffectCoverflow, Pagination } from 'swiper/modules';
-import SectionHeader from '../SectionHeader/SectionHeader.jsx';
+import { EffectCoverflow, Pagination } from "swiper/modules";
+import SectionHeader from "../SectionHeader/SectionHeader.jsx";
 
-
-//declaring variables 
-
-
+//declaring variables
 
 export default function Project() {
+  const [category, setCategory] = useState([projects]);
+  useEffect(() => {
+    projects().then((data) => {
+      setCategory(data);
+    });
+  }, []);
 
-  const [category, setCategory] = useState(projects);
   const handleFilter = (e) => {
     let word = e.target.value;
-    console.log(word)
+    console.log(word);
 
-    if (word == 'All') {
+    if (word == "All") {
       setCategory(projects);
     }
 
-    if (word == 'WebDesign') {
-      const filtered = projects.filter(item => item.type === "WebDesign");
-      setCategory(filtered)
-      console.log(filtered)
-        ;
+    if (word == "WebDesign") {
+      const filtered = projects.filter((item) => item.type === "WebDesign");
+      setCategory(filtered);
+      console.log(filtered);
     }
 
-    if (word == 'Graphics') {
-      const filtered = projects.filter(item => item.type === "Graphics");
-      setCategory(filtered)
-      console.log(filtered)
-        ;
+    if (word == "Graphics") {
+      const filtered = projects.filter((item) => item.type === "Graphics");
+      setCategory(filtered);
+      console.log(filtered);
     }
 
-
-    if (word == 'Apps') {
-      const filtered = projects.filter(item => item.type === "Apps");
-      setCategory(filtered)
-      console.log(filtered)
-        ;
+    if (word == "Apps") {
+      const filtered = projects.filter((item) => item.type === "Apps");
+      setCategory(filtered);
+      console.log(filtered);
     }
+  };
 
-
-  }
   return (
-
-    <div id='project'>
+    <div id="project">
       <SectionHeader title="My Projects" />
-      <div className='nav_filterbuttons'>
-
-        <button className="filterbutton" value="All" onClick={handleFilter}>All</button>
-        <button className="filterbutton" value="WebDesign" onClick={handleFilter}>WebDesign</button>
-        <button className="filterbutton" value="Graphics" onClick={handleFilter}>Graphics</button>
-        <button className="filterbutton" value="Apps" onClick={handleFilter}>Apps</button>
-
-
-
+      <div className="nav_filterbuttons">
+        <button className="filterbutton" value="All" onClick={handleFilter}>
+          All
+        </button>
+        <button
+          className="filterbutton"
+          value="WebDesign"
+          onClick={handleFilter}
+        >
+          WebDesign
+        </button>
+        <button
+          className="filterbutton"
+          value="Graphics"
+          onClick={handleFilter}
+        >
+          Graphics
+        </button>
+        <button className="filterbutton" value="Apps" onClick={handleFilter}>
+          Apps
+        </button>
       </div>
       <div className="projectswiper">
         <Swiper
-          effect={'coverflow'}
+          effect={"coverflow"}
           grabCursor={true}
           centeredSlides={true}
-          slidesPerView={'auto'}
+          slidesPerView={"auto"}
           coverflowEffect={{
             rotate: 50,
             stretch: 0,
@@ -88,33 +93,20 @@ export default function Project() {
           modules={[EffectCoverflow, Pagination]}
           className="mySwiper"
         >
-
           {category.map((project) => (
-
-            <div key={project.id} className='projectcard'>
-
+            <div key={project.id} className="projectcard">
               <SwiperSlide>
-
-                <img src={project.img_url} />
+                <img src={`http://localhost:8000/storage/${project.img_url}`} />
                 <h2>{project.name}</h2>
                 <p>{project.description}</p>
-                <button>github</button>
+                <a href={project.link}>
+                  <button> github</button>
+                </a>
               </SwiperSlide>
-
-
-
             </div>
-
           ))}
         </Swiper>
       </div>
-
-
-
-
-
-
     </div>
-
-  )
+  );
 }
