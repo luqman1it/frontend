f9db94ab9518a1d6332955936b6859379daaf415
abdullatif <abdullatifjr002@gmail.com>
2024-05-12 21:38:47 +0300

    changed add project to a form

--- src/Component/Dashboard/DashboardProject/sendData.js
+++ src/Component/Dashboard/DashboardProject/sendData.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const sendData = async (data) => {
-  return await axios.post('/http://127.0.0.1:8000/api/addprojects', {
+  return await axios.post('http://127.0.0.1:8000/api/addprojects', {
     name: data.name,
     description: data.description,
     img_url: data.imgUrl,
@@ -14,7 +14,7 @@ export const sendData = async (data) => {
   })
   .then(function (response) {
     console.log(response);
-    console.log("send")
+    console.log("sent successfully")
   })
   .catch(function (error) {
     console.log(error);
