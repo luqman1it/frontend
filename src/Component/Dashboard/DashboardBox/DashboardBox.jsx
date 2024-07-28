c99e97dd2139133a2f40714416098f4dc99cb40f
AYA2300 <ayasalleh2300@gmail.com>
2024-07-28 11:14:16 +0300

    aya

--- src/Component/Dashboard/DashboardBox/DashboardBox.jsx
+++ src/Component/Dashboard/DashboardBox/DashboardBox.jsx
@@ -1,15 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './DashboardBox.css'
+import axios from 'axios';
 export default function DashboardBox() {
+    const[data,setData]=useState({projects:'',skills:'',Messages:''});
+
+    
+  useEffect(() => {
+    const CountsData = async () => {
+      try {
+        const response = await axios.get(`http://localhost:8000/api/count`, {
+         
+        });
+        const counts= response.data.count;
+        console.log(counts);
+        setData(counts);
+        
+  
+      } catch (error) {
+        console.error('Error fetching user data', error);
+      }
+    };
+   
+  
+    CountsData();
+  }, []);
+  
+  
+  
+
+
+
+
     return (
         <div className='dash-box'>
             <div className="box">
                 <h1>Projects</h1>
-                <span>100</span>
+                <span>{data.projects}</span>
+            </div>
+            <div className="box">
+                <h1>Skilles</h1>
+                <span>{data.skills}</span>
             </div>
             <div className="box">
                 <h1>Message</h1>
-                <span>100</span>
+                <span>{data.Messages}</span>
             </div>
         </div>
     )
