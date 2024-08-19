54bdde11b745317ed7b004a421705eb3d991a267
kenana-mohammad <kenana4324@gmail.com>
2024-08-19 21:01:34 +0300

    toast

--- src/Component/Dashboard/DashboardProject/ShowForm/ShowForm.jsx
+++ src/Component/Dashboard/DashboardProject/ShowForm/ShowForm.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from 'react'
 import './ShowForm.css'
 import axios from 'axios';
 import DashboardProject from '../DashboardProject'
-
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const ShowForm = () => {
 
